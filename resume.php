81edec8f80952786d918b5c6a4a8b843daea782a
matsong23 <gmatson2023@gmail.com>
2025-01-19 01:17:36 -0500

    Update resume.php

--- resume.php
+++ resume.php
@@ -1,2 +1,27 @@
 <?php
+<!DOCTYPE html>
+<html>
+
+<head>
+<style>
+h1 {text-align: center;}
+</style>
+</head>
+
+<body>
+
+<!-- name and contact info -->
+<h1>Gunnar Matson</h1><br><br>
+<p1>15361 Obenhoff Rd Atlantic Mine, MI 49905</p1><br>
+<p2>(906) 281-7022</p2><br>
+<p3>gmatson2023@gmail.com</p3><br>
+
+<!-- college info -->
+<!-- languages -->
+<!-- tools -->
+<!-- previous important classes -->
+<!-- current class -->
+
+</body>
+</html>
 ?>
